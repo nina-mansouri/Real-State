924f04c3073dac365ccf3cdc59fcb8fc2ac5b415
Nina <n.mansouri2486@gmail.com>
2025-08-01 23:49:18 +0330

    Update page.js

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -5,9 +5,24 @@ async function BuyResidentials({ searchParams }) {
   const res = await fetch(`${process.env.BASE_URL}/api/profile`, {
     cache: "no-store",
   });
-  const data = await res.json();
+  const contentType = res.headers.get("content-type");
+  let data = null;
 
-  if (data.error) return <h3>مشکلی پیش آمده است</h3>;
+  try{
+    if (contentType && contentType.includes("application/json")) {
+      const data = await res.json();
+    } else {
+      const text = await res.text();
+      console.error("پاسخ JSON نبود:", text);
+       return <h3>مشکلی پیش آمده است</h3>;
+    }
+  } catch (error) {
+    console.error("خطا در JSON()", error);
+    return <h3>مشکلی پیش آمده است(parse JSON failed)</h3>;
+  }
+  
+
+  if (data?.error) return <h3>مشکلی پیش آمده است</h3>;
 
   let finalData = data.data;
   if (searchParams.category) {
